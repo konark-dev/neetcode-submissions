8587046087bb0fdee305c049bc22c33aa084b242
konark-dev <konarkagarwal9999@gmail.com>
2026-09-18 16:43:14 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> u1;
+        for(char c : s) {
+            u1[c]++;
+        }
+        unordered_map<char, int> u2;
+        for(char c : t) {
+            u2[c]++;
+        }
+
+        if(u1 == u2) {
+            return true;
+        }
+
+        return false;
+    }
+};
